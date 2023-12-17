7a9c4c84f77b01f741db009b275d4a251dc599b3
Palatip Jantawong <palatipjant@outlook.com>
2023-12-17 23:08:46 +0700

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Palatip Jantawong on 17/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
